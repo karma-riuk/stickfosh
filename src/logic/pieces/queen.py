bb0a3266c7b44507ee8798120b1e287105337858
Karma Riuk <riukkarma@gmail.com>
2025-01-30 17:12:53 +0100

    implemented queen legal moves

--- src/logic/pieces/queen.py
+++ src/logic/pieces/queen.py
@@ -1,5 +1,31 @@
 from .piece import Piece
 
 class Queen(Piece):
-    pass
+    def legal_moves(self, board: "Board") -> list[Move]:
+        ret = []
 
+        # looking north east
+        ret.extend(self._look_direction(board, 1, 1))
+
+        # looking south east
+        ret.extend(self._look_direction(board, 1, -1))
+
+        # looking south west
+        ret.extend(self._look_direction(board, -1, -1))
+
+        # looking north west
+        ret.extend(self._look_direction(board, -1, 1))
+
+        # looking east
+        ret.extend(self._look_direction(board, 1, 0))
+
+        # looking south
+        ret.extend(self._look_direction(board, 0, -1))
+
+        # looking west
+        ret.extend(self._look_direction(board, -1, 0))
+
+        # looking north
+        ret.extend(self._look_direction(board, 0, 1))
+
+        return ret
