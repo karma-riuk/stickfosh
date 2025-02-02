95327ec65308838546ed00771884a16448c56b25
Karma Riuk <riukkarma@gmail.com>
2025-02-02 18:17:19 +0100

    cleaned up main

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -4,15 +4,7 @@
 
 int main(int argc, char* argv[]) {
     std::string pos =
-        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
-    Board* b = Board::setup_fen_position(pos);
-
-    std::string fen = b->to_fen();
-
-    std::cout << pos << std::endl;
-    std::cout << fen << std::endl;
-
-    std::cout << (fen.substr(0, fen.find(" ")) == pos.substr(0, pos.find(" ")))
-              << std::endl;
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    Board b = Board::setup_fen_position(pos);
     return 0;
 }
