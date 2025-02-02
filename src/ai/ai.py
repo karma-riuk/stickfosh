166e1c76648ca8bdf466d1534ea0174e1882450a
Karma Riuk <riukkarma@gmail.com>
2025-02-02 13:18:25 +0100

    added some positions to test the move generation

--- src/ai/ai.py
+++ src/ai/ai.py
@@ -22,14 +22,38 @@ pos2expected = {
         3: 97_862,
         4: 4_085_603,
     },
-    # pos 2 after f3f6
-    "r3k2r/p1ppqpb1/bn2pQp1/3PN3/1p2P3/2N4p/PPPBBPPP/R3K2R b KQkq - 0 1": {
-        2: 2_111,
-        3: 77_838,
+
+    # -- Position 3
+    "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1": {
+        1: 14,
+        2: 191,
+        3: 2_812,
+        4: 43_238,
+        5: 674_624,
     },
-    # pos 2 after f3f6 and e7d8
-    "r2qk2r/p1pp1pb1/bn2pQp1/3PN3/1p2P3/2N4p/PPPBBPPP/R3K2R w KQkq - 1 2": {
-        2: 1843,
+
+    # -- Position 4
+    "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1": {
+        1: 6,
+        2: 264,
+        3: 9467,
+        4: 422_333,
+    },
+
+    # -- Position 5
+    "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8": {
+        1: 44,
+        2: 1486,
+        3: 62379,
+        4: 2103487,
+    },
+
+    # -- Position 6
+    "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10": {
+        1: 46,
+        2: 2079,
+        3: 89890,
+        4: 3894594,
     },
 }
 
--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ from ai.ai import peft
 if __name__ == "__main__":
     board = INITIAL_BOARD
 
-    pos = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
+    pos = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"
     board = Board.setup_FEN_position(pos)
 
     view = GUI()
