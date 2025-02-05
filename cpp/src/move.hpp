3f3017a38997cf044292a15afe98e9f14ed27426
Karma Riuk <riukkarma@gmail.com>
2025-02-05 14:41:23 +0100

    removed useless comments

--- cpp/src/move.hpp
+++ cpp/src/move.hpp
@@ -11,9 +11,6 @@ struct Move {
     int8_t source_square;
     int8_t target_square;
 
-    // bool is_capturing = false;
-    // CastleSide castle_side = CastleSide::NeitherSide;
-    // bool en_passant = false;
     int8_t promoting_to = Piece::None;
 
     std::string to_string() const {
