e821814cd12af83ed623f467e824d438ecbdab44
Karma Riuk <riukkarma@gmail.com>
2025-02-06 10:43:28 +0100

    updated the code of main

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,11 +1,14 @@
-#include "stickfosh.hpp"
+#include "ais/ai.hpp"
 
 #include <iostream>
 
 int main(int argc, char* argv[]) {
     std::string pos =
         "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-    std::string move = search(pos, 4);
+
+    ai::v1_simple ai;
+
+    std::string move = ai.search(pos, 4);
     std::cout << move << std::endl;
     return 0;
 }
