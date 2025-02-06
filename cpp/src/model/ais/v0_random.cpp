74fa99fe7bad045b57b152d72c0a365bd99d1349
Karma Riuk <riukkarma@gmail.com>
2025-02-06 23:46:06 +0100

    faking some thought for the random player

--- cpp/src/model/ais/v0_random.cpp
+++ cpp/src/model/ais/v0_random.cpp
@@ -5,5 +5,8 @@
 Move ai::v0_random::_search(const Board& b) {
     std::vector<Move> moves = b.all_legal_moves();
 
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(10)); // Simulate work
+
     return moves[rand() % moves.size()];
 }
