88b2d01cf8804b1b3492275e53f48b7d901a5b7b
Karma Riuk <riukkarma@gmail.com>
2025-02-07 10:03:37 +0100

    made v1_simple multithreaded again

--- cpp/src/model/ais/v1_simple.cpp
+++ cpp/src/model/ais/v1_simple.cpp
@@ -1,5 +1,8 @@
 #include "../pieces/piece.hpp"
+#include "../utils/threadpool.hpp"
 #include "ai.hpp"
+
+#include <map>
 
 
 static int INFINITY = std::numeric_limits<int>::max();
@@ -7,16 +10,24 @@ static int INFINITY = std::numeric_limits<int>::max();
 int position_counter;
 
 Move ai::v1_simple::_search(const Board& b) {
+    position_counter = 0;
+    ThreadPool pool(std::thread::hardware_concurrency());
+
     std::vector<Move> moves = b.all_legal_moves();
 
-    position_counter = 0;
+    std::map<Move, std::future<int>> futures;
+    for (const Move& move : moves) {
+        Board tmp_board = b.make_move(move);
+        futures.insert({move, pool.enqueue([&, tmp_board]() {
+                            return _search(tmp_board, 3);
+                        })});
+    }
 
     Move best_move;
     int best_eval = -INFINITY;
     int counter = 0;
-    for (const auto& move : moves) {
-        Board tmp_board = b.make_move(move);
-        int eval = _search(tmp_board, 3);
+    for (auto& [move, future] : futures) {
+        int eval = future.get();
         counter++;
         if (!am_white)
             eval *= -1;
