1d83c066f6bf89b21ad4ee19842b68b25749c181
Karma Riuk <riukkarma@gmail.com>
2025-02-07 09:51:22 +0100

    made v0_random thinking longer

--- cpp/src/model/ais/v0_random.cpp
+++ cpp/src/model/ais/v0_random.cpp
@@ -6,7 +6,8 @@ Move ai::v0_random::_search(const Board& b) {
     std::vector<Move> moves = b.all_legal_moves();
 
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(10)); // Simulate work
+    std::this_thread::sleep_for(std::chrono::milliseconds(thinking_time)
+    ); // Simulate work
 
     return moves[rand() % moves.size()];
 }
