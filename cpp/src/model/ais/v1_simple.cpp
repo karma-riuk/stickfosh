7e888bba61215e4c46d5160e06bea839943ac7a9
Karma Riuk <riukkarma@gmail.com>
2025-02-07 09:51:49 +0100

    made v1_simple single threaded because it was getting messy

--- cpp/src/model/ais/v1_simple.cpp
+++ cpp/src/model/ais/v1_simple.cpp
@@ -1,30 +1,23 @@
 #include "../pieces/piece.hpp"
-#include "../utils/threadpool.hpp"
 #include "ai.hpp"
 
-#include <map>
 
 static int INFINITY = std::numeric_limits<int>::max();
 
 int position_counter;
 
 Move ai::v1_simple::_search(const Board& b) {
-    ThreadPool pool(std::thread::hardware_concurrency());
-
     std::vector<Move> moves = b.all_legal_moves();
 
-    std::map<Move, std::future<int>> futures;
-    for (const Move& move : moves) {
-        Board tmp_board = b.make_move(move);
-        futures.insert({move, pool.enqueue([&, tmp_board]() {
-                            return _search(tmp_board, 3);
-                        })});
-    }
+    position_counter = 0;
 
     Move best_move;
     int best_eval = -INFINITY;
-    for (auto& [move, future] : futures) {
-        int eval = future.get();
+    int counter = 0;
+    for (const auto& move : moves) {
+        Board tmp_board = b.make_move(move);
+        int eval = _search(tmp_board, 3);
+        counter++;
         if (!am_white)
             eval *= -1;
         if (eval > best_eval) {
